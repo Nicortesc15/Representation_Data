eda8837fcbc74f69026321bb7b9346f0871bb2f8
Nicolas Cortes <nicortesc1525@gmail.com>
2024-11-25 23:49:12 +0100

    updated swiss roll

--- src/task2_diffusion_maps/2_swiss_roll.py
+++ src/task2_diffusion_maps/2_swiss_roll.py
@@ -11,17 +11,24 @@ import numpy.typing as npt
 
 # TODO: Generate swiss roll dataset
 data_points = 5000
-swiss_roll_data, t= make_swiss_roll(data_points, noise = 0.0)
+swiss_roll_data, t = make_swiss_roll(data_points, noise = 0.0)
 
 # TODO: Visualize data-set
-fig = plt.figure(figsize=(10, 10))
+elevation = 10  # Vertical angle (degrees)
+azimuth = 78    # Horizontal angle (degrees)
+
+fig = plt.figure(figsize=(8, 8))
 ax = plt.axes(projection="3d")
-scatter = ax.scatter(swiss_roll_data[:, 0], swiss_roll_data[:, 1], swiss_roll_data[:, 2], s=1, alpha=0.5)
+ax.view_init(elev=elevation, azim=azimuth)
+scatter = ax.scatter(swiss_roll_data[:, 0], swiss_roll_data[:, 1], swiss_roll_data[:, 2], c = t ,s=10, alpha=0.8)
 ax.set_title("Swiss Roll")
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
 plt.show()
 
 # TODO: Use function diffusion_map() defined in utils to compute first ten eigenfunctions (corresponding to 10 largest eigenvalues) of the Laplace Beltrami operator on the “swiss roll” manifold
-n_eig = 10
+n_eig = 12
 lambda_, phi_l = utils.diffusion_map(swiss_roll_data, n_eig)
 
 # TODO: Plot the first non-constant eigenfunction φ1 against the other eigenfunctions
@@ -29,23 +36,22 @@ lambda_, phi_l = utils.diffusion_map(swiss_roll_data, n_eig)
 # Plot of first non-constant eigenfunction φ1 against the other eigenfunctions
 for i in range(2,n_eig):
     plt.figure()
-    plt.scatter(phi_l[:, 1], phi_l[:, i], s=1, alpha=0.5)
+    plt.scatter(phi_l[:, 1], phi_l[:, i], c = t,s=1, alpha=0.5)
     plt.title(f'phi_1 vs phi_{i}')
     plt.xlabel('phi_1')
     plt.ylabel(f'phi_{i}')
     plt.show()
 
 # PCA computation
-swiss_roll_data_centered = utils.center_data(swiss_roll_data)       # centered data
-U, S, V_t = utils.compute_svd(swiss_roll_data_centered)             # Decompose swiss roll into singular vectors U, V and values S
+U, S, V_t = utils.compute_svd(swiss_roll_data)                      # Decompose swiss roll into singular vectors U, V and values S
 n_components = [2,3]                                                # Number of principal components
 
 # Plots with n principal components
 for n in n_components:
     data_reconstructed = utils.reconstruct_data_using_truncated_svd(U, S, V_t,n)
-    fig = plt.figure(figsize=(10, 10))
+    fig = plt.figure(figsize=(8, 8))
     ax = plt.axes(projection="3d")
-    ax.scatter(data_reconstructed[:, 0], data_reconstructed[:, 1], data_reconstructed[:, 2], s=1, alpha=0.5)
+    ax.scatter(data_reconstructed[:, 0], data_reconstructed[:, 1], data_reconstructed[:, 2], c = t ,s=10, alpha=0.8)
     ax.set_title(f'First {n} Principal Components')
     plt.show()
 
