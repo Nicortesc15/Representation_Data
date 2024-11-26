05a1d2f61a32ddf650a4a90c0127024636af00b1
Nicolas Cortes <nicortesc1525@gmail.com>
2024-11-26 21:18:44 +0100

    Rescale function implemented

--- src/task3_vae/2_fire_evac.py
+++ src/task3_vae/2_fire_evac.py
@@ -1,6 +1,6 @@
 import torch
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, TensorDataset
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
 from model import VAE
@@ -14,10 +14,56 @@ Not included in automated tests, as it's open-ended.
 """
 
 # TODO: Download the FireEvac dataset 
+train_dataset = np.load('data/FireEvac_train_set.npy')
+test_dataset = np.load('data/FireEvac_test_set.npy')
+
+print(train_dataset.shape)
+print(test_dataset.shape)
 
 # TODO: Make a scatter plot to visualise it.
+plt.scatter(train_dataset[:, 0], train_dataset[:, 1], alpha=0.5, label='Training Data')
+plt.scatter(test_dataset[:, 0], test_dataset[:, 1], alpha=0.5, label='Test Data')
+plt.title("FireEvac Dataset")
+plt.xlabel("x-position")
+plt.ylabel("y-position")
+plt.legend()
+plt.show()
+
+# TODO: Rescale data
+train_dataset_scaled = rescale_data(train_dataset)
+test_dataset_scaled = rescale_data(test_dataset)
 
 # TODO: Train a VAE on the FireEvac data
+
+# TODO: Set the learning rate, batch size and no. of epochs
+learning_rate = 0.005
+batch_size = 64      
+epochs = 200 
+
+# TODO: Create an instance of Dataloader for train_dataset using torch.utils.data, use appropriate batch size, keep shuffle=True.
+train_loader = DataLoader(torch.tensor(train_dataset_scaled, dtype=torch.float32), batch_size=64, shuffle=False)
+
+# TODO: Create an instance of Dataloader for test_dataset using torch.utils.data, use appropriate batch size, keep shuffle=False.
+test_loader = DataLoader(torch.tensor(test_dataset_scaled, dtype=torch.float32), batch_size=64, shuffle=False)
+
+# TODO: Set dimensions: input dim, latent dim, and no. of neurons in the hidden layer
+d_in = 2
+d_latent = 2
+d_hidden_layer = 64
+
+# TODO: Instantiate the VAE model with a latent dimension of 2, using the utility function instantiate_vae() from utils
+vae = instantiate_vae(d_in, d_latent, d_hidden_layer, device)
+
+# TODO. Set up an appropriate optimizer from torch.optim with an appropriate learning rate
+optimizer = optim.Adam(vae.parameters(), lr = learning_rate)
+
+plots_at_epochs = []  # generate plots at epoch numbers
+
+# TODO: Compute train and test losses by performing the training loop using the utility function training_loop() from utils
+train_losses, test_losses = training_loop(vae, optimizer, train_loader, test_loader, epochs,plots_at_epochs, device)
+
+# TODO: Plot the loss curve using the utility function plot_loss() from utils
+plot_loss(train_losses, test_losses)
 
 # TODO: Make a scatter plot of the reconstructed test set
 
